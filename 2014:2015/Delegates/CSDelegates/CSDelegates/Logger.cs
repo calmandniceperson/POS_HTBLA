e367e5342bdc866f33e368477fe51013f6e016cd
mkocs <mko.mko31@gmail.com>
2014-10-16 09:56:40 +0200

    Added delegates exercise

---
+++ 2014:2015/Delegates/CSDelegates/CSDelegates/Logger.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CSDelegates
+{
+	public class Logger
+	{
+		public Logger ()
+		{
+		}
+
+		/*
+		 * logs the current time + a message
+		 */
+		public static string log(string text){
+			return (DateTime.Now.ToShortTimeString()  + " " + text);
+		}
+	}
+}
+
---
+++ 2014:2015/Delegates/CSDelegates/CSDelegates/Program.cs
@@ -0,0 +1,55 @@
+﻿/*
+ * MICHAEL KOEPPL
+ * 2013/2014
+ * CSDelegates
+ */
+
+using System;
+
+namespace CSDelegates
+{
+	class MainClass
+	{
+		/*
+		 * delegate
+		 */
+		public delegate string LogDel(string txt);
+
+		public static void Main (string[] args)
+		{
+			string ant = string.Empty, ant1;
+
+			/*
+			 * set LogDel to null for now
+			 */
+			LogDel l = null;
+
+			do {
+				Console.Clear();
+
+				Console.Write("Wollen Sie loggen? (j/n) ");
+				ant1 = Console.ReadLine();
+
+				switch(ant1.ToLower()){
+					
+				case "j":
+					/*
+					 * calls the method Logger.log(); with the delegate
+					 * and prints the returned string
+					 */
+					l = Logger.log;
+					Console.WriteLine(l("Hier ist Ihr Log."));
+					break;
+
+				case "n":
+					l = null;
+					break;
+
+				}
+
+				Console.Write("Wollen Sie das Programm neu starten? (j/n) ");
+				ant = Console.ReadLine();
+			} while(ant.ToLower ().StartsWith ("j"));
+		}
+	}
+}
