1b040dd073e47485aafb02bd4520b24cc7927044
Michael Köppl <mko.mko31@gmail.com>
2015-02-10 19:09:06 +0100

    Add stack events exceptions exercise in C#

---
+++ 2014:2015/StackEventsExceptions/CSStackEventsException/CSStackEventsException/CStack.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CSStackEventsException
+{
+	public delegate void StackDelegate();
+
+	public class CStack
+	{
+		List<String> Stack;
+
+		public int capacity{ get; private set; }
+
+		public CStack (int cap /*capacity*/)
+		{
+			capacity = cap;
+			Stack = new List<String> (cap);
+		}
+
+		public void push(string element){
+			// Abfrage: Kapazitaet erreicht?
+			// ja: -> Exception ausloesen
+			// nein: -> Element eintragen
+			if(Stack.Capacity < Stack.Count + 1){
+				throw new CStackOverflowException ();
+			}else{
+				Stack.Add (element);
+			}
+		}
+
+		public string pop(){
+			// Abfrage: noch ein Element in der Liste vorhanden?
+			// ja: -> Element returnen
+			// nein: -> Event ausloesen
+
+			if(Stack.Count != 0){
+				return Stack.Last();
+			}else{
+				underFlow ();
+			}
+
+			return string.Empty;
+		}
+
+		public event StackDelegate underFlow;
+	}
+
+	class CStackOverflowException:ApplicationException{
+		// -> 3 Konstruktoren!
+
+		public CStackOverflowException(){
+		}
+
+		public CStackOverflowException(string message):base(message){
+		}
+
+		public CStackOverflowException(string message, Exception inner):base(message, inner){
+		}
+	}
+}
+
---
+++ 2014:2015/StackEventsExceptions/CSStackEventsException/CSStackEventsException/Program.cs
@@ -0,0 +1,72 @@
+﻿/*
+ * MICHAEL KOEPPL 3AHIF
+ * Stack Beispiel
+ * 
+ * Angabe:
+ * Es soll ein Stack in einer Klasse gehalten werden (mit Methoden push & pop).
+ * Der Klasse muss eine maximale Groesse fuer den Stack mitgegeben werden
+ * Wenn die maximale Groesse ueberschritten wird, soll eine OverflowException geworfen werden,
+ * wenn sie unterschritten wird, soll ein Event ausgeloest werden
+ * 
+ * Fertigstellung: 10.02.2015
+ */
+using System;
+
+namespace CSStackEventsException
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string ant = string.Empty;
+			do {
+				try {
+					Console.WriteLine ("Geben Sie die Kapazitaet der Liste ein: ");
+					var cap = int.Parse (Console.ReadLine ());
+
+					var myStack = new CStack (cap);
+					myStack.underFlow += myStack_underflow;
+
+
+
+					do {
+						switch(menu()){
+						case 1:
+							Console.WriteLine("Geben Sie Ihre Nachricht ein, die angehaengt werden soll: ");
+							myStack.push(Console.ReadLine());
+							break;
+						case 2:
+							Console.WriteLine(myStack.pop());
+							break;
+						}
+
+						Console.WriteLine ("Wollen Sie noch eine Aktion ausfuehren? (j/n) ");
+						ant = Console.ReadLine ();
+					} while(ant.ToLower ().StartsWith ("j"));
+					//myStack.push("Test");
+					//myStack.push("lel");
+					Console.WriteLine (myStack.pop ());
+				} catch (CStackOverflowException) {
+					Console.WriteLine ("Stack Overflow");
+				} catch (Exception ex) {
+					Console.WriteLine (ex.Message);
+				}					
+
+				Console.WriteLine("Wollen Sie das Programm neu starten? (j/n) ");
+				ant = Console.ReadLine();
+			} while(ant.ToLower ().StartsWith ("j"));
+		}
+
+		static void myStack_underflow(){
+			throw new NotImplementedException ();
+		}
+
+		static int menu(){
+			Console.Clear ();
+			Console.WriteLine ("1 ... Push Element");
+			Console.WriteLine ("2 ... Pop Element");
+
+			return int.Parse (Console.ReadLine ());
+		}
+	}
+}
