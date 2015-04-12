692161a109885b3e1f9c19d33c03a15548a1bc64
Michael Koeppl <mko.mko31@gmail.com>
2015-04-12 14:23:36 +0200

    Added Cpp calculations training

---
+++ Cpp/simplecalculations/simplecalc.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int fn, sn;
+
+	cout << "Enter your first number: ";
+	cin >> fn;
+
+	cout << "Enter the second number: ";
+	cin >> sn;
+
+	cout << "Sum: " << fn+sn << "\n";
+	cout << "Subtract: " << fn-sn << "\n";
+	cout << "Multiplication: " << fn*sn << "\n";
+
+	if(fn%sn != 0){ /*just a little extra*/
+		cout << "Division: " << fn/sn << " (rounded)\n";
+	}else{
+		cout << "Division: " << fn/sn << "\n";
+	}
+	cout << "Modulo: " << fn%sn << "\n";
+
+	return 0;
+}
