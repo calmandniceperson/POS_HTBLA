377e06eb9bd3c672a73c50ada7bdf1539a07b1dd
Michael Köppl <mko.mko31@gmail.com>
2015-05-10 15:57:36 +0200

    Added cross-platform Eto HelloWorld app in C#

---
+++ CS/EtoHelloWorld/EtoHelloWorld.Desktop/Program.cs
@@ -0,0 +1,19 @@
+﻿// /*
+//  *	MICHAEL KOEPPL 3AHIF
+//  * 	2014/2015
+//  */
+using System;
+using Eto.Forms;
+
+namespace EtoHelloWorld.Desktop
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			new Application (Eto.Platform.Detect).Run (new MainForm ());
+		}
+	}
+}
+
---
+++ CS/EtoHelloWorld/EtoHelloWorld.XamMac2/Program.cs
@@ -0,0 +1,19 @@
+﻿// /*
+//  *	MICHAEL KOEPPL 3AHIF
+//  * 	2014/2015
+//  */
+using System;
+using Eto.Forms;
+
+namespace EtoHelloWorldXamMac2
+{
+	public class Program
+	{
+		[STAThread]
+		public static void Main (string[] args)
+		{
+			new Application (Eto.Platforms.XamMac2).Run (new EtoHelloWorld.MainForm ());
+		}
+	}
+}
+
