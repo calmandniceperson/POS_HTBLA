4de41776e1841f3973c3601ba3df0d6aaef2ce52
Michael Koeppl <mko.mko31@gmail.com>
2015-05-31 15:41:50 +0200

    Added 1.5 exercise (separate output)

---
+++ Cpp/CppPrimer/Exercises/1_5_SeparatePrint.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+	std::cout << "Enter two numbers: " << std::endl;
+	int v1 = 0, v2 = 0;
+	std::cin >> v1 >> v2;
+	std::cout << "First number: " << v1 << std::endl;
+	std::cout << "Second number: " << v2 << std::endl;
+	std::cout << "Product: " << v1 + v1 << std::endl;
+	return 0;
+}
