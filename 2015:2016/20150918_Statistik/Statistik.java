890c11e551f26a12aa7afa2c094f05c0caf6b678
Michael Köppl <mko.mko31@gmail.com>
2015-09-18 14:26:02 +0200

    Add new Statistik example

---
+++ 2015:2016/20150918_Statistik/Statistik.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.*;
+
+public class Statistik {
+  public static void main(String[]args) {
+    Scanner s = new Scanner(System.in);
+    System.out.print("Geben Sie ein: ");
+    int eingabe = s.nextInt();
+
+    int numArray[] = new int[10];
+    int highest = -1;
+    for(int i = 0; i < eingabe; i++) {
+      int rand = getZufall();
+      numArray[rand]++;
+      if(numArray[rand] > highest) {
+        highest = numArray[rand];
+      }
+    }
+    if(highest > 50) {
+      highest = highest / 50;
+    } else {
+      highest = 1;
+    }
+    for(int i = 0; i <= 9; i++) {
+      System.out.print(i+":");
+      int limit;
+      if(numArray[i] != highest) {
+        limit = numArray[i]/highest;
+      } else {
+        limit = highest;
+      }
+      for(int j = 0; j < limit; j++) {
+        if(numArray[i] > 0) {
+          System.out.print("X");
+        }
+      }
+      System.out.println();
+    }
+    System.out.println("Legende: ein X entspricht " + highest + " Treffer(n).");
+  }
+
+  static int getZufall() {
+    Random r = new Random();
+    return r.nextInt(10 - 0) + 0;
+  }
+}
