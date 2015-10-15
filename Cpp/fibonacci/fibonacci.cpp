6712b7526085a0a628f91f71d48af70118a68e4c
Michael Köppl <mko.mko31@gmail.com>
2015-10-15 12:33:49 +0200

    Update fibonacci example

--- Cpp/fibonacci/fibonacci.cpp
+++ Cpp/fibonacci/fibonacci.cpp
@@ -2,20 +2,14 @@
 #import <vector>
 
 int main(int argc, char* argv[]) {
-  std::vector<unsigned long long> nums;
-  nums.push_back(0);
-  nums.push_back(1);
-  nums.push_back(1);
-  std::cout << 0 << "\n" << 1 <<  "\n" << 1 << std::endl;
-  int index = nums.size();
-  while(index < 100) {
-    try {
-      //std::cout << index << std::endl;
-      nums.push_back(nums.at(index-1) + nums.at(index-2));
-    } catch(std::out_of_range o){
-      std::cout<<o.what()<<std::endl;
-    }
-    std::cout << nums.at(index) << std::endl;
+  int index = 0;
+  int firstN = 0, secondN = 1, newN;
+  std::cout << firstN << "\n" << secondN << std::endl;
+  while(index < 35) {
+    newN = firstN + secondN;
+    std::cout << newN << std::endl;
+    firstN = secondN;
+    secondN = newN;
     index++;
   }
   return 0;
