2b3a47f19c67984a2d94f11f5384b4179e7dc016
Michael Köppl <mko.mko31@gmail.com>
2015-12-13 14:47:22 +0100

    Update Java emacs file read example The program now also writes to a file

--- Java/ReadTest.java
+++ Java/ReadTest.java
@@ -2,23 +2,31 @@
 // written with emacs
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 
 public class ReadTest {
     public static void main(String[] args)  throws IOException {
         BufferedReader br = null;
+        BufferedWriter bw = null;
         try {
             br = new BufferedReader(new FileReader("test.txt"));
+            bw = new BufferedWriter(new FileWriter("output.txt"));
             String txt;
             while((txt = br.readLine()) != null) {
                 System.out.println(txt);
+                bw.write(txt+"\n");
             }
         } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
             if(br != null) {
                 br.close();
+            }
+            if(bw != null) {
+                bw.close();
             }
         }
     }
