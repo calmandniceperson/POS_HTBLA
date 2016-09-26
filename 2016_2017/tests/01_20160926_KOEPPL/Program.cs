7491dd7a08dcb411418cd088151b4fe7c1f982a6
Michael Köppl <mko.mko31@gmail.com>
2016-09-26 11:33:47 +0200

    Add 2016/2017 tests

---
+++ 2016_2017/tests/01_20160926_KOEPPL/Program.cs
@@ -0,0 +1,43 @@
+﻿// Michael Koeppl 5AHIF
+// 26 September 2016
+
+using System;
+using System.IO;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        private static double storageUsage;
+        private static void DoSubDir(DirectoryInfo dirInfo)
+        {
+            FileInfo[] fInfo = dirInfo.GetFiles();
+            if (fInfo.Length == 0)
+                return;
+            foreach(FileInfo f in fInfo)
+            {
+                // macOS fix
+                if (f.Name != ".DS_Store")
+                {
+                    Console.WriteLine(f.Name);
+                    storageUsage += f.Length;
+                }
+            }
+            var subDirInfo = dirInfo.GetDirectories();
+            if (subDirInfo.Length == 0)
+                return;
+            foreach (DirectoryInfo inf in subDirInfo)
+            {
+                DoSubDir(inf);
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            Console.Clear();
+            DirectoryInfo dInfo = new DirectoryInfo("datavz/");
+            DoSubDir(dInfo);
+            Console.WriteLine("\n" + storageUsage + " bytes");
+        }
+    }
+}
